d3aa5f990f0fd010c577cfa64eee942066fb8b9e
Thái Chí Bảo <comaybayy@gmail.com>
2022-06-05 16:20:57 +0700

    add fonts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
-<h1 class="py-2 bg-indigo-800 text-white text-center font-bold text-6xl mx-28">CMB</h1>
+<h1 class="font-comfortaa py-2 bg-indigo-800 text-white text-center text-6xl mx-28">Cỏ May Bay</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>Hello</p>
 <img src="/favicon.png" alt="fav" />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['OpenSans', 'serif'],
+      comfortaa: ['Comfortaa'],
+    },
   },
   plugins: [],
 }
