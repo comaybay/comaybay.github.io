ec1f311a502b0f405abf3a4a98a6975de27fd3f1
Thái Chí Bảo <comaybayy@gmail.com>
2022-06-24 11:44:38 +0700

    code formating config

--- svelte.config.js
+++ svelte.config.js
@@ -3,29 +3,29 @@ import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true,
-		}),
-	],
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 
-	kit: {
-		adapter: adapter(),
-		appDir: "app",
-		prerender: {
-			default: true
-		},
-		// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-		routes: (filepath) => ![
-			// default config
-			/(?:(?:^_|\/_)|(?:^\.|\/\.)(?!well-known))/,
-			
-			// exclude files inside directories named "components"
-			/components\//
-		].some(regex => regex.test(filepath)),
-	}
+  kit: {
+    adapter: adapter(),
+    appDir: "app",
+    prerender: {
+      default: true
+    },
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    routes: (filepath) => ![
+      // default config
+      /(?:(?:^_|\/_)|(?:^\.|\/\.)(?!well-known))/,
+
+      // exclude files inside directories named "components"
+      /components\//
+    ].some(regex => regex.test(filepath)),
+  }
 };
 
 export default config;
