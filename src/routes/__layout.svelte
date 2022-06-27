a0e0c59cb2a5dd84e53df613a0a37c8749fe17c5
Thái Chí Bảo <comaybayy@gmail.com>
2022-06-27 17:01:56 +0700

    update header padding when breakpoint changes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
+	import { browser } from '$app/env';
 	import { onMount } from 'svelte';
 
 	import '../tailwind.css';
 	import Header, { getHeight } from './components/Header.svelte';
 
 	let style = '';
-	onMount(() => (style = `padding-top: ${getHeight()}px;`));
+	const updateStyle = () => (style = `padding-top: ${getHeight()}px;`);
+	onMount(updateStyle);
+
+	//update style when breakpoint changes
+	if (browser) {
+		//equivalent of 'md' tailwind
+		matchMedia('(min-width: 768px)').addEventListener('change', updateStyle);
+	}
 </script>
 
 <Header />
