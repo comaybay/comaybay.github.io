42a8f7bcecbea5c8e6f4fd9d2b207fb985c72c29
Thái Chí Bảo <comaybayy@gmail.com>
2022-06-27 16:20:43 +0700

    component used  to center content

---
+++ src/routes/components/ContentCenter.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let className = '';
+</script>
+
+<div class="mx-auto max-w-5xl {className}">
+	<slot />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,9 @@
-<h1 class="font-comfortaa py-2 bg-indigo-800 text-white text-center text-6xl mx-28">Cỏ May Bay</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>Hello</p>
-<img src="/favicon.png" alt="fav" />
+<script>
+	import ContentCenter from './components/ContentCenter.svelte';
+</script>
+
+<ContentCenter className="mt-4">
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<p>Hello</p>
+	<img src="/favicon.png" alt="fav" />
+</ContentCenter>
