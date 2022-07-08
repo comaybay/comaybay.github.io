546eeee6272004ab71945983accb8b2d5c78ee3d
Thái Chí Bảo <62052850+comaybay@users.noreply.github.com>
2022-07-08 11:45:00 +0700

    Theme selection (#4)
    * move script in app.html to external file
    
    * dynamically change theme in SelectTheme
    
    * enable dark mode

--- src/routes/components/settingsComponents/SelectTheme.svelte
+++ src/routes/components/settingsComponents/SelectTheme.svelte
@@ -20,6 +20,15 @@
 	function changeTheme(theme: Theme) {
 		activeTheme = theme;
 		localStorage.setItem('theme', theme);
+
+		if (
+			theme === 'dark' ||
+			(theme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
 	}
 </script>
 
