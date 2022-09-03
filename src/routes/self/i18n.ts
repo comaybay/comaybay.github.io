2cf2261058f9a08576c8d4391f1d430d17181cf2
Thái Chí Bảo <comaybayy@gmail.com>
2022-09-03 15:28:56 +0700

    add socials for mobile for page self, update page i18n

--- src/routes/self/i18n.ts
+++ src/routes/self/i18n.ts
@@ -9,7 +9,7 @@ export default createI18nStore<IndexPage>({
 		dontExpectQuality:
 			'Chất lượng của thứ mình tạo ra có thể sẽ không được hoàn hảo cho lắm do mình là một người đi ngược lại với câu "1 nghề cho chín còn hơn 9 nghề" nhưng hi vọng bạn vẫn tìm được thứ gì đó thú vị từ các sản phẩm của mình!',
 		ideaToReality: 'Khi có ý tưởng nào đó khiến nổi hứng lên, ăn ngủ không yên thì mình sẽ cố biến nó thành sự thật!!',
-		checkOutMyStuff: 'Bạn có thể liên hệ mình hoặc xem những thứ mình làm tại:'
+		checkOutMyStuff: 'Bạn có thể xem những thứ mình làm tại:'
 	},
 	en: {
 		greetings: 'Hi there!!',
@@ -19,7 +19,7 @@ export default createI18nStore<IndexPage>({
 		dontExpectQuality: `Quality of my works might not be perfect because I'm more of a "jack of all trades master of none" type of guy but I hope you can still find something enjoyable about my works!`,
 		ideaToReality:
 			'When a new and interesting idea strikes and starts living rent-free in my head, I will try to make it a reality!!',
-		checkOutMyStuff: 'You can contact me or check out my stuff at:'
+		checkOutMyStuff: 'You can check out my stuff at:'
 	}
 });
 
--- src/routes/self/index.svelte
+++ src/routes/self/index.svelte
@@ -1,17 +1,17 @@
 <script>
 	import Content from '../components/Content.svelte';
 	import SocialsDesktop from './components/SocialsDesktop.svelte';
+	import SocialsMobile from './components/SocialsMobile.svelte';
 	import t from './i18n';
 </script>
 
-<Content className="md:mt-10 font-light md:text-lg ">
+<Content className="md:mt-10 font-light md:text-lg">
 	<p class="md:text-xl">{$t.greetings}</p>
-	<p class="md:ml-10 mb-5 md:mb-10 md:text-xl">{$t.introduction}</p>
-	<p class="">{$t.whatICreate}</p>
+	<p class="md:ml-10 md:text-xl">{$t.introduction}</p>
+	<p class="mt-5 md:mt-10">{$t.whatICreate}</p>
 	<p class="mt-3">{$t.dontExpectQuality}</p>
 	<p class="mt-3">{$t.ideaToReality}</p>
-	<p class="mt-12">{$t.checkOutMyStuff}</p>
+	<p class="mt-5 md:mt-10">{$t.checkOutMyStuff}</p>
+	<SocialsMobile />
 </Content>
-<div class="mt-8 pb-8">
-	<SocialsDesktop />
-</div>
+<SocialsDesktop />
