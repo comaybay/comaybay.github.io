657951f30a505f7eb0cf93656abaf15acde0ffff
comaybay <comaybayy@gmail.com>
2023-08-16 22:23:46 +0700

    update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,41 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
-import path from 'path';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [
-    preprocess({
-      postcss: true
-    })
-  ],
+  preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter({
       precompress: true
     }),
-    appDir: 'app',
-    prerender: {
-      default: true
-    },
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    routes: filepath =>
-      ![
-        // default config
-        /(?:(?:^_|\/_)|(?:^\.|\/\.)(?!well-known))/,
-
-        // exclude files inside directories named "components"
-        /components(.*)\//
-      ].some(regex => regex.test(filepath)),
-    vite: {
-      resolve: {
-        alias: {
-          // these are the aliases and paths to them
-          src: path.resolve('./src')
-        }
-      }
+    alias: {
+      src: "src"
     }
   }
 };
